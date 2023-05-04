7d5ec34b6cd63e83c00804b2d6f87d7e953b4afd
Nisha Ahirwar <nishaahirwar.work@gmail.com>
2023-05-05 01:03:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+
+  const [buttonText, setButtonText]=useState(["get Bitcoin price"])
+  var [count, setCount]=useState(0);
+  var cur=200;
+  var pre=100;
+
+  function handleClick(e){
+    setCount(count+1)
+    setButtonText("refresh bitcoin price");
+    e.target.style.background="purple";
+    
+    
+  }
+  return(
+  <div>
+    {(count>=1) && <h1>Current Price {cur}</h1> }
+    {(count>=2) && <h1>Previous price {pre}</h1>}
+      
+     
+
+    
+    
+    <button style={{background: "blue"}} onClick={handleClick}>{buttonText}</button>
+
+  </div>)
 }
 
 export default App;
